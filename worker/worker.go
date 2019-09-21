a189c5b3c8e047f0b5dadbc3e2ab1a6a87280dc1
Alexander Ponomarev <shootnix@gmail.com>
2019-09-22 00:05:45 +0600

    add new worker - checker

--- worker/worker.go
+++ worker/worker.go
@@ -20,6 +20,11 @@ type Reporter struct {
 	Name         string
 }
 
+type Checker struct {
+	timeInterval time.Duration
+	Name         string
+}
+
 type Worker interface {
 	Run()
 }
@@ -40,6 +45,14 @@ func NewReporter(name string, timeInt time.Duration) *Reporter {
 	return w
 }
 
+func NewChecker(name string, timeInt time.Duration) *Checker {
+	w := new(Checker)
+	w.Name = name
+	w.timeInterval = timeInt
+
+	return w
+}
+
 func NewWorker(wName, wType string, timeInt time.Duration) Worker {
 	log := logger.GetLogger()
 
@@ -50,6 +63,8 @@ func NewWorker(wName, wType string, timeInt time.Duration) Worker {
 		return NewReporter(wName, timeInt)
 	case "poster":
 		return NewPoster(wName, timeInt)
+	case "checker":
+		return NewChecker(wName, timeInt)
 	default:
 		log.Fatal("Unknown worker type: " + wType)
 		return nil
@@ -93,6 +108,40 @@ func (w *Reporter) Run() {
 			log.Fatal("Can't insert message: " + err.Error())
 		}
 		m.Send("Telegram")
+	}
+}
+
+// Перезабрасывает в очередь письма,
+// которые не отправились почему-то
+func (w *Checker) Run() {
+	log := logger.GetLogger()
+	sql := `
+
+		SELECT id 
+		  FROM messages
+		 WHERE is_success = -1
+		 ORDER BY ctime DESC
+
+	`
+
+	rows, err := io.GetPg().Conn.Query(sql)
+	if err != nil {
+		log.Error("Can't execute query " + sql + ": " + err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int64
+		if err := rows.Scan(&id); err != nil {
+			log.Error("Can't get message id: " + err.Error())
+			continue
+		}
+		m, err := entity.GetMessage(id)
+		if err != nil {
+			log.Error("Can't get message by id: " + err.Error())
+			continue
+		}
+		m.Send("telegram")
 	}
 }
 
